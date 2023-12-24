311886d053f229d4dbed2bc60ee8b11f165b2686
kyle-salaysay <foursalaysay@gmail.com>
2023-12-25 00:12:24 +0800

    redefined code

--- src/components/User/UserAppointment.tsx
+++ src/components/User/UserAppointment.tsx
@@ -1,8 +1,26 @@
 'use client'
 
 import { ScrollArea } from "@/components/ui/scroll-area"
-import UserAppointmentStatus from "./UserAppointmentStatus"
 import { Service } from '@/lib/Types'
+
+
+import {
+    Card,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+
+  import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+  
 
 
 
@@ -10,7 +28,46 @@ import { Service } from '@/lib/Types'
 export default function UserAppointment({services} : {services : Service[]}){
     return(
         <ScrollArea className="w-full">
-            <UserAppointmentStatus services={services}/>
+            <div>
+            {services ?  (services.map((getService) => (
+                <Card 
+                className="w-full h-20 flex flex-row gap-2"
+                key={getService.serviceId}> 
+                    <CardHeader>
+                        <CardTitle>{getService.serviceName}</CardTitle>
+                        <CardDescription>Card Description</CardDescription>
+                    </CardHeader>
+                    
+                    <CardFooter>
+                        <Dialog>
+                            <DialogTrigger>More Info</DialogTrigger>
+                            <DialogContent>
+                                <DialogHeader>
+                                <DialogTitle>More Details</DialogTitle>
+                                <DialogDescription>
+                                    <div>
+                                        <p>{getService.providerName}</p>
+                                        <p>{getService.providerAddress}</p>
+                                        <p>{getService.providerContactNumber}</p>
+                                    </div>
+                                    <div>
+                                        <p>{getService.date}</p>
+                                        <p>{getService.status}</p>
+                                    </div>
+                                </DialogDescription>
+                                </DialogHeader>
+                            </DialogContent>
+                            </Dialog>
+                    </CardFooter>
+                </Card>
+           
+            ))) : (
+                <div className='flex items-center justify-center'>
+                        <h1>No Services Available</h1>
+                </div>
+            
+            )}
+        </div>
 
         </ScrollArea>
     )
